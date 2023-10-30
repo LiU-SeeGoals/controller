9446a8d06fb945a8fb63d98461506a3b4389e6f1
jakos322 <jakos322@student.liu.se>
2023-10-30 17:25:53 +0100

    no idea what changed here

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
+
 import (
 	"fmt"
+	"github.com/LiU-SeeGoals/controller/examples"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	// SSLVisionExample()
+	examples.SSLVisionExample()
 }
