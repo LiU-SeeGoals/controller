41d8f1c8c66c0f0007b5564e1cdb2e0239834403
Oskar Arensmeier <oskar.arensmeier@outlook.com>
2024-01-18 16:32:20 +0100

    install go deps on container build, added build script, added dockerignore

--- scripts/compose_build.sh
+++ scripts/compose_build.sh
@@ -1,6 +1,6 @@
-# Take down docker environment 
+# Build docker environment
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SRC_DIR="${SCRIPT_DIR}/../"
 
-cd $SRC_DIR && COMPOSE_PROJECT_NAME=seegoals docker compose down
+cd $SRC_DIR && COMPOSE_PROJECT_NAME=seegoals docker compose build
