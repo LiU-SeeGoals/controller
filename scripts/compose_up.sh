9c8a0a2e933fee068542e4de8854318a1f7ff172
jakos322 <jakos322@student.liu.se>
2024-05-08 19:48:59 +0200

    updated readme, removed unused files

--- scripts/compose_up.sh
+++
@@ -1,6 +0,0 @@
-# Start docker environment in detached mode
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SRC_DIR="${SCRIPT_DIR}/../"
-
-cd $SRC_DIR && COMPOSE_PROJECT_NAME=seegoals docker compose up -d
