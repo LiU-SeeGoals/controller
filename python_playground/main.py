fix force calculation in apply_force_based_on_distance function

python_playground/main.py
@@ -12,7 +12,7 @@ CORS(app)
 model = SeeGoalsDNN(num_players_per_team=2)
 if pathlib.Path(model.path).exists():
     print("Loading model")
-    model.load_state_dict(torch.load(model.path))
+    model.load_state_dict(torch.load(model.path, weights_only=False))
     model.eval()
 calls = 0
 start = time.perf_counter()
@@ -49,31 +49,6 @@ def slow_brin_blue():
     print(f"CPS: {cps:.2f}")
     return jsonify(plan), 200
 
-@app.route('/slowBrainYellow', methods=['POST'])
-def slow_brin_yellow():
-    gamestate = GameState(request.get_json())
-    my_team = gamestate.yellow_teams
-    enemy_team = gamestate.blue_teams
-    ball = gamestate.ball
-    scale = 100
-    output = model(my_team.to_torch().unsqueeze(0), enemy_team.to_torch().unsqueeze(0), ball.to_torch().unsqueeze(0)) * scale
-    instructions = []
-
-    for pred, robot in zip(output[0], my_team.robots.values()):
-        dx, dy = pred.tolist()
-        dest = [robot.position.x + dx, robot.position.y + dy, 0, 0]
-
-        instructions.append({
-            "Id": robot.id,
-            "Position": dest
-        })
-    
-
-    plan = {
-        "Instructions": instructions,
-    }
-    print(plan)
-    return jsonify(plan), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)

python_playground/models.py
@@ -29,7 +29,11 @@ class SeeGoalsDNN(nn.Module):
         self.num_frequencies = num_frequencies
         self.num_players_per_team = num_players_per_team
         self.freq_bands = torch.linspace(1.0, 2 ** (num_frequencies - 1), num_frequencies, requires_grad=False)
-        self.norm_factor=torch.tensor([field_hight, field_hight], requires_grad=False).float()
+        if field_hight > field_width:
+            self.norm_factor=torch.tensor([field_hight, field_hight], requires_grad=False).float()
+        else:
+            self.norm_factor=torch.tensor([field_width, field_width], requires_grad=False).float()
+            
         input_size = (num_players_per_team * 2 * 2) + 2  # my team + enemy team + ball position
         enriched_size = input_size * num_frequencies * 2
         self.layers = nn.Sequential(

python_playground/train.py
@@ -7,15 +7,15 @@ from models import SeeGoalsDNN
 def apply_force_based_on_distance(ball, team):
     """Apply a force to the ball based on the distance to the team."""
     # Compute vectors from team players to the ball
-    vec = team - ball
-    max_force = 10
-    decrease_factor = 0.9995
+    vec = ball - team
+    max_force = 100
+    decrease_factor = 0.999
     dist = torch.norm(vec, dim=2)  
     # Compute scaling factor based on the distance
-    force = (max_force * decrease_factor ** dist).unsqueeze(2)
+    force = (max_force * (decrease_factor ** dist)).unsqueeze(2)
     team_force = torch.mean(force, dim=1).unsqueeze(1)
     # Update ball positions by adding the computed force
-    new_pos_ball = ball + force  
+    new_pos_ball = ball + (vec / dist.unsqueeze(2)) * team_force
     return new_pos_ball
 
 def ball_to_target_loss(my_team, delta, ball, target = torch.tensor([0.0, 0.0])):
@@ -36,6 +36,7 @@ def ball_to_target_loss(my_team, delta, ball, target = torch.tensor([0.0, 0.0]))
     ball_loss = torch.mean(ball_dist_after_force - ball_to_target)
     team_to_ball_loss = torch.mean(predicted_team_to_ball - original_team_to_ball)
     return team_to_ball_loss + ball_loss
+    # return ball_loss
 
     
 def train(model = None, 
@@ -44,7 +45,7 @@ def train(model = None,
         players_per_team = 2,
         batch_size = 256,
         lr = 0.0001,
-        epochs = 20_000,
+        epochs = 50_000,
         loss_fn = ball_to_target_loss,
         load_model = False
         ):
@@ -52,7 +53,7 @@ def train(model = None,
     if model is None:
         model = SeeGoalsDNN(num_players_per_team=players_per_team, field_hight=field_hight, field_width=field_width)
     if load_model:
-        model.load_state_dict(torch.load(model.path))
+        model.load_state_dict(torch.load(model.path, weights_only=False))
         model.eval()
         return model
     optimizer = optim.Adam(model.parameters(), lr=lr)