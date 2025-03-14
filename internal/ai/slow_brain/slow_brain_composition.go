8613b5cadafd0a2419ce7865b9a1f201c34bdc9b
jakos322 <jakos322@student.liu.se>
2025-03-14 17:58:32 +0100

    ref kicker progress

--- internal/ai/slow_brain/slow_brain_composition.go
+++ internal/ai/slow_brain/slow_brain_composition.go
@@ -1,6 +1,7 @@
 package ai
 
 import (
+	"fmt"
 	"sync"
 	"time"
 
@@ -27,6 +28,7 @@ type SlowBrainComposition struct {
 	activities       *[info.TEAM_SIZE]ai.Activity // <-- pointer to the slice
 	activity_lock    *sync.Mutex                  // shared mutex for synchronization
 	prev_ref         bool
+	waiting_for_kick bool
 }
 
 func (m *SlowBrainComposition) ClearActivities() {
@@ -50,6 +52,9 @@ func (m *SlowBrainComposition) ReplaceActivities(activities *[info.TEAM_SIZE]ai.
 }
 
 func (m *SlowBrainComposition) HandleRef(gi *info.GameInfo, active_robots []int) bool {
+
+	fmt.Println(gi.Status.GetGameEvent().GetCurrentState())
+
 	switch gi.Status.GetGameEvent().GetCurrentState() {
 	case info.STATE_HALTED, info.STATE_STOPPED, info.STATE_PENALTY_PREPARATION, info.STATE_FREE_KICK, info.STATE_TIMEOUT, info.STATE_BALL_PLACEMENT:
 		for _, value := range active_robots {
@@ -57,17 +62,29 @@ func (m *SlowBrainComposition) HandleRef(gi *info.GameInfo, active_robots []int)
 		}
 		m.prev_ref = true
 		return true
+
 	case info.STATE_KICKOFF_PREPARATION:
 		if gi.Status.GetGameEvent().GetTeamWithPossession() == m.team { // We are kicker
-			m.AddActivity(ai.NewRefKicker(info.ID(0), m.team))
+			m.AddActivity(ai.NewRefKicker(info.ID(1), m.team))
 			// m.AddActivity(ai.NewRefKickoff(info.ID(0), m.team))
 		} else { // We are not kicker
-			m.AddActivity(ai.NewRefKickoff(info.ID(0), m.team))
+			m.AddActivity(ai.NewRefKickoff(info.ID(1), m.team))
 		}
+		m.waiting_for_kick = true
 		m.prev_ref = true
 		return true
 
 	default:
+		if m.waiting_for_kick {
+			if gi.State.Ball.GetVelocity().Norm() > 10 {
+				m.waiting_for_kick = false
+				m.prev_ref = false
+				return false
+			} else {
+				m.prev_ref = true
+				return true
+			}
+		}
 		// If we are exiting ref activity
 		if m.prev_ref == true {
 			m.ClearActivities()
