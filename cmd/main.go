537aa97f04694fa64bc7ae9814ca5afcdf1c8b8b
Rasmus Wallin <...>
2025-03-23 17:03:48 +0100

    added argument to running main to launch specific scenarios

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/LiU-SeeGoals/controller/internal/demos"
+import (
+	"fmt"
+	"os"
+
+	"github.com/LiU-SeeGoals/controller/internal/demos"
+)
 
 func main() {
+	if len(os.Args) > 1 {
+		if os.Args[1] == "webdev" {
+			fmt.Println("Starting webdev...")
+			demos.RwSimScenario()
+			return
+		}
+	}
+
 	demos.FwRealScenario()
 }
